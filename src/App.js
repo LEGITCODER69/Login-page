fa029f252fcab8f843a20e76fcf048673940b202
worldtourmydream@gmail.com <worldtourmydream@gmail.com>
2023-09-17 20:16:53 +0530

    now

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-import React from 'react'; // Make sure to import React
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div>
       <section>
-        <div className="form-box"> {/* Use className instead of class */}
-          <div className="form-value"> {/* Use className instead of class */}
+        <div className="form-box">
+          <div className="form-value">
             <form action="">
               <h2>Login</h2>
-              <div className="inputbox"> {/* Use className instead of class */}
+              <div className="inputbox">
                 <ion-icon name="mail-outline"></ion-icon>
                 <input type="email" required />
-                <label htmlFor="">Email</label> {/* Use htmlFor instead of for */}
+                <label htmlFor="">Email</label>
               </div>
-              <div className="inputbox"> {/* Use className instead of class */}
+              <div className="inputbox">
                 <ion-icon name="lock-closed-outline"></ion-icon>
                 <input type="password" required />
-                <label htmlFor="">Password</label> {/* Use htmlFor instead of for */}
+                <label htmlFor="">Password</label>
               </div>
-              <div className="forget"> {/* Use className instead of class */}
-                <label htmlFor="">
+              <div className="forget">
+                <label>
                   <input type="checkbox" />Remember Me{' '}
-                  <a href="#">Forget Password</a>
+                  <button type="button">Forget Password</button>
                 </label>
               </div>
               <button>Log in</button>
-              <div className="register"> {/* Use className instead of class */}
+              <div className="register">
                 <p>
-                  Don't have an account <a href="#">Register</a>
+                  Don't have an account <button type="button">Register</button>
                 </p>
               </div>
             </form>
