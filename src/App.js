c788beb55d52972d87ae94f6ef036727337c6c9d
worldtourmydream@gmail.com <worldtourmydream@gmail.com>
2023-09-17 19:20:43 +0530

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react'; // Make sure to import React
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <div className="form-box"> {/* Use className instead of class */}
+          <div className="form-value"> {/* Use className instead of class */}
+            <form action="">
+              <h2>Login</h2>
+              <div className="inputbox"> {/* Use className instead of class */}
+                <ion-icon name="mail-outline"></ion-icon>
+                <input type="email" required />
+                <label htmlFor="">Email</label> {/* Use htmlFor instead of for */}
+              </div>
+              <div className="inputbox"> {/* Use className instead of class */}
+                <ion-icon name="lock-closed-outline"></ion-icon>
+                <input type="password" required />
+                <label htmlFor="">Password</label> {/* Use htmlFor instead of for */}
+              </div>
+              <div className="forget"> {/* Use className instead of class */}
+                <label htmlFor="">
+                  <input type="checkbox" />Remember Me{' '}
+                  <a href="#">Forget Password</a>
+                </label>
+              </div>
+              <button>Log in</button>
+              <div className="register"> {/* Use className instead of class */}
+                <p>
+                  Don't have an account <a href="#">Register</a>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        nomodule
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+      ></script>
     </div>
   );
 }
